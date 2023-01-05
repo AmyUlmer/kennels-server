f0ee2c18e126de630189adf6f9adc46cd89ece30
Amy Ulmer <aulmer703@gmail.com>
2023-01-05 16:53:07 -0600

    first commit

---
+++ views/animal_requests.py
@@ -0,0 +1,31 @@
+ANIMALS = [
+    {
+        "id": 1,
+        "name": "Snickers",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 4
+    },
+    {
+        "id": 2,
+        "name": "Roman",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 2
+    },
+    {
+        "id": 3,
+        "name": "Blue",
+        "species": "Cat",
+        "locationId": 2,
+        "customerId": 1
+    }
+]
+
+
+def get_all_animals():
+    #This Python module has one method defined in it.
+    #Want to make that method available to any other Python code.
+    #To do that, you import it into the __init__.py module.
+    "Method -- gets all objects in the array"
+    return ANIMALS
